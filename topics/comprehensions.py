39da52314957d4b8ba394d576e8383490bfc03bc
devdanig <danielgfdev@gmail.com>
2026-01-22 17:16:50 -0500

    Update: For anidados y Comprehensions

--- topics/comprehensions.py
+++ topics/comprehensions.py
@@ -75,8 +75,37 @@ def extract_sources(articles):
         if article.get('source') and article.get('source').get('name')
     }
 
-print(f'Con For titles: {extract_titles_for(sample_articles)}') 
-print(f'Con Comprehension titles: {extract_titles(sample_articles)}') 
-print(f'Titulo y descripcion con Comprenhesions: {extract_title_and_description(sample_articles)}')
-print(f'Fuentes con for: {extract_source_for(sample_articles)}')
-print(f'Fuente con comprehensions: {extract_sources(sample_articles)}')
+# Categorizar todas las noticias deacuerdo a la categoria para tener una lista de las noticias que hacen match con esta categoria
+
+def categorizar_for(articles):
+    sources = extract_sources(articles)
+    results = {}
+    for source in sources:
+        if source not in results:
+            results[source] = []
+        for article in articles:
+            if source == article.get('source').get('name'):
+                results[source].append(article)
+    return results
+
+
+def categorizar(articles):
+    sources = extract_sources(articles)
+    return {
+        source: [
+            article
+            for article in articles
+            if source == article.get('source').get('name')
+        ]
+        for source in sources
+    }
+
+
+# print(f'Con For titles: {extract_titles_for(sample_articles)}') 
+# print(f'Con Comprehension titles: {extract_titles(sample_articles)}') 
+# print(f'Titulo y descripcion con Comprenhesions: {extract_title_and_description(sample_articles)}')
+# print(f'Fuentes con for: {extract_source_for(sample_articles)}')
+# print(f'Fuente con comprehensions: {extract_sources(sample_articles)}')
+# print(f'Catogorias: {categorizar_for(sample_articles)}')
+print(f'Categorizacion:{categorizar_for(sample_articles)}')
+print(f'Categorizacion Comprehensions: {categorizar(sample_articles)}')
